781617adea812d3ff08120fa58cb088d2909a6cd
neverw1nter77 <neverw1nter100@outlook.com>
2026-08-24 08:31:28 +0700

    Добавление плагинов

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,5 +1,12 @@
 import pytest
-from playwright.sync_api import Playwright, Page
+from playwright.sync_api import Page, Playwright
+
+
+@pytest.fixture
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
 
 
 @pytest.fixture(scope="session")
@@ -7,6 +14,7 @@ def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
+
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
@@ -20,6 +28,7 @@ def initialize_browser_state(playwright: Playwright):
 
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
+
     context.storage_state(path="browser-state.json")
     browser.close()
 
@@ -28,6 +37,5 @@ def initialize_browser_state(playwright: Playwright):
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json")
-    page = context.new_page()
-    yield page
+    yield context.new_page()
     browser.close()
