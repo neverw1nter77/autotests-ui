a6f17b9c629153e69d62eb6d23b9cd8585cbc572
neverw1nter77 <neverw1nter100@outlook.com>
2026-09-11 18:34:29 +0700

    add test_edit_course

--- pages/authentication/registration_page.py
+++ pages/authentication/registration_page.py
@@ -1,3 +1,5 @@
+import re
+
 from playwright.sync_api import Page
 from components.authentication.registration_form_component import RegistrationFormComponent
 from pages.base_page import BasePage
@@ -13,6 +15,10 @@ class RegistrationPage(BasePage):
 
         self.login_link = Link(page, 'registration-page-login-link', 'Login')
         self.registration_button = Button(page, 'registration-page-registration-button', 'Registration')
+
+    def click_login_link(self):
+        self.login_link.click()
+        self.check_current_url(re.compile(".*/#/auth/login"))
 
     def click_registration_button(self):
         self.registration_button.click()
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,7 @@
-from playwright.sync_api import Page
+from typing import Pattern
+
+from playwright.sync_api import Page, expect
+
 
 class BasePage:
     def __init__(self, page: Page):
@@ -8,4 +11,7 @@ class BasePage:
         self.page.goto(url, wait_until='networkidle')
 
     def reload(self):
-        self.page.reload(wait_until='networkidle')
+        self.page.reload(wait_until='domcontentloaded')
+
+    def check_current_url(self, expected_url: Pattern[str]):
+        expect(self.page).to_have_url(expected_url)
