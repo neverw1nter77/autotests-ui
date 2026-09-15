68bb21be24d1b0a55a75bc7f3a4a26464d62980e
neverw1nter77 <neverw1nter100@outlook.com>
2026-09-15 09:08:12 +0700

    add allure annotations

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,12 +1,26 @@
 import pytest
-
+import allure
 from pages.courses.courses_list_page import CoursesListPage
 from pages.courses.create_course_page import CreateCoursePage
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
+from allure_commons.types import Severity
 
 
 @pytest.mark.courses
 @pytest.mark.regression
+@allure.tag(AllureTag.REGRESSION, AllureTag.COURSES)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.COURSES)
+@allure.story(AllureStory.COURSES)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.COURSES)
+@allure.sub_suite(AllureStory.COURSES)
 class TestCourses:
+    @allure.title("Check displaying of empty courses list")
+    @allure.severity(Severity.NORMAL)
     def test_empty_courses_list(self, courses_list_page: CoursesListPage):
         courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
         courses_list_page.navbar.check_visible('username')
@@ -14,6 +28,8 @@ class TestCourses:
         courses_list_page.toolbar_view.check_visible()
         courses_list_page.check_visible_empty_view()
 
+    @allure.title("Create course")
+    @allure.severity(Severity.CRITICAL)
     def test_create_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
         create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
         create_course_page.create_course_toolbar_view.check_visible()
@@ -41,18 +57,11 @@ class TestCourses:
             index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
         )
 
+    @allure.title("Edit course")
+    @allure.severity(Severity.CRITICAL)
     def test_edit_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
         create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
-        create_course_page.create_course_toolbar_view.check_visible()
-        create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
-        create_course_page.create_course_form.check_visible(
-            title="", estimated_time="", description="", max_score="0", min_score="0"
-        )
-
-        create_course_page.create_course_exercises_toolbar_view.check_visible()
-        create_course_page.check_visible_exercises_empty_view()
-
-        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+        create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
         create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
         create_course_page.create_course_form.fill(
             title="Playwright",
@@ -68,22 +77,17 @@ class TestCourses:
             index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
         )
         courses_list_page.course_view.menu.click_edit(index=0)
-        create_course_page.create_course_form.check_visible(
-            title="Playwright",
-            estimated_time="2 weeks",
-            description="Playwright",
-            max_score="100",
-            min_score="10"
-        )
+
         create_course_page.create_course_form.fill(
-            title="Python",
-            estimated_time="5 weeks",
-            description="Python",
-            max_score="50",
-            min_score="5"
+            title="New Playwright",
+            estimated_time="3 weeks",
+            description="New Playwright",
+            max_score="1000",
+            min_score="100"
         )
         create_course_page.create_course_toolbar_view.click_create_course_button()
+
         courses_list_page.toolbar_view.check_visible()
         courses_list_page.course_view.check_visible(
-            index=0, title="Python", max_score="50", min_score="5", estimated_time="5 weeks"
+            index=0, title="New Playwright", max_score="1000", min_score="100", estimated_time="3 weeks"
         )
