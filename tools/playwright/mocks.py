a756ff32baac8169b55697c7244c51e054834ba5
neverw1nter77 <neverw1nter100@outlook.com>
2026-09-22 09:39:29 +0700

    add logging

--- tools/playwright/mocks.py
+++ tools/playwright/mocks.py
@@ -1,12 +1,5 @@
-from playwright.sync_api import Page, Route
-
-
-def abort(route: Route):
-    # Временно печатаем ссылку на отключаемый ресурс
-    print(f'\nAborting url: {route.request.url}')
-    route.abort()  # Отменяем загрузку ресурса
+from playwright.sync_api import Page
 
 
 def mock_static_resources(page: Page):
-    # Отключаем загрузку статических ресурсов
-    page.route("**/*.{ico,png,jpg,webp,mp3,mp4,woff,woff2}", abort)
+    page.route("**/*.{ico,png,jpg,webp,mp3,mp4,woff,woff2}", lambda route: route.abort())
